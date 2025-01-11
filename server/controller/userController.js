16fd6a3266a6382783391d9c69a32b644c5042cc
ShadyKahouech <shady.kahouech@gmail.com>
2025-01-11 15:57:17 +0100

    update user

--- server/controller/userController.js
+++ server/controller/userController.js
@@ -58,85 +58,41 @@ const deleteUser = async (req, res) => {
   }
 };
 
-// const SignIn = async (req, res) => {
-//   try {
-//     const { firstName, lastName, email, password, role, image } = req.body;
-
-//     // Email validation
-//     const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-//     if (!emailRegex.test(email)) {
-//       return res.status(400).json({ message: "Invalid email format" });
-//     }
-
-//     if (!image && role === "prestataire") {
-//       return res.status(400).send("At least 1 images are required.");
-//     }
-
-//     let uploadedPromises = cloudinary.uploader
-//       .upload(image, {
-//         cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
-//         api_key: process.env.CLOUDINARY_API_KEY,
-//         api_secret: process.env.CLOUDINARY_API_SECRET,
-//         resource_type: "auto",
-//       })
-//       .then((result) => {
-//         console.log("Image uploaded successfully:", result.secure_url);
-//       })
-//       .catch((error) => {
-//         console.error("Error uploading image:", error);
-//       });
-
-//     const uploadedResults = await Promise.all(uploadedPromises);
-//     console.log("uploaded images:", uploadedResults);
-
-//     // Password validation
-//     const isPasswordValid = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])[^_\s]{6,}$/.test(
-//       password
-//     );
-//     if (!isPasswordValid) {
-//       return res.status(400).json({
-//         message:
-//           "Password must be at least 6 characters long and include at least one uppercase letter, one lowercase letter, and one digit.",
-//       });
-//     }
-//     // Check if the email is already used
-//     const userExist = await User.findOne({ where: { email } });
-//     if (userExist) {
-//       return res.status(400).json({ message: "Email is already in use" });
-//     }
-
-//     // Create a new user
-//     const hashedPassword = await bcrypt.hash(password, 10);
-//     const user = await User.create({
-//       firstName,
-//       lastName,
-//       email,
-//       role,
-//       image: uploadedResults,
-//       password: hashedPassword,
-//     });
-//     // Generate JWT this the parameter that we can include when we generate the token for the user
-//     const token = jwt.sign(
-//       {
-//         id: user.id,
-//         firstName: user.firstName,
-//         lastName: user.lastName,
-//         email: user.email,
-//         role: user.role,
-//         image: user.image,
-//       },
-//       secret,
-//       { expiresIn: "1h" }
-//     );
-
-//     return res.status(201).json({ token, message: "Sign In successful", user });
-//   } catch (error) {
-//     console.error(error);
-//     return res
-//       .status(500)
-//       .json({ message: "Server Error", error: error.message });
-//   }
-// };
+const updateUser = async (req, res) => {
+  const { id } = req.params;
+  const { firstName, lastName, email, image, password } = req.body;
+  try {
+    let imageUrl;
+    if (image) {
+      const uploadedImage = await cloudinary.uploader.upload(image, {
+        cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+        api_key: process.env.CLOUDINARY_API_KEY,
+        api_secret: process.env.CLOUDINARY_API_SECRET,
+        resource_type: "auto",
+      });
+      imageUrl = uploadedImage.secure_url; // Save the uploaded image URL
+    }
+    const [update] = await User.update(
+      {
+        firstName,
+        lastName,
+        email,
+        password,
+        image: imageUrl ? [imageUrl] : undefined,
+      },
+      { where: { userId: id } }
+    );
+    if (update) {
+      res.status(200).send({ message: "User updated successfully" });
+    } else {
+      res.status(404).send({ message: "User is not fount" });
+    }
+    const bcrypt = require("bcrypt");
+  } catch (error) {
+    console.error(error);
+    res.status(500).send("Error updating the product");
+  }
+};
 
 // verify token
 const verifyToken = (req, res, next) => {
@@ -281,4 +237,5 @@ module.exports = {
   SignIn,
   verifyToken,
   loginUser,
+  updateUser,
 };
--- server/router/userRouter.js
+++ server/router/userRouter.js
@@ -6,6 +6,7 @@ const {
   SignIn,
   verifyToken,
   loginUser,
+  updateUser,
 } = require("../controller/userController");
 
 const route = express.Router();
@@ -15,4 +16,5 @@ route.get("/getoneuser", getOneUser);
 route.delete("/deleteuser/:id", deleteUser);
 route.post("/signin", SignIn);
 route.post("/loginUser", loginUser);
+route.put("/updateuser/:id", updateUser);
 module.exports = route;
